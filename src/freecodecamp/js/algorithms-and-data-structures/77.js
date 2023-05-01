ddb2efd13d21c1f0dece57d8e863acb07bd04aba
a-andrievskikh <sn0abg0@gmail.com>
2023-05-02 02:44:59 +0500

    chore: сomplete task 77 for Algorithms and Data Structures

---
+++ src/freecodecamp/js/algorithms-and-data-structures/77.js
@@ -0,0 +1,28 @@
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+
+  switch (val) {
+    case "a":
+      answer = "apple";
+      return answer;
+      break;
+    case "b":
+      answer = "bird";
+      return answer;
+      break;
+    case "c":
+      answer = "cat";
+      return answer;
+      break;
+    default:
+      answer = "stuff";
+      return answer;
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
