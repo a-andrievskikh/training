598b4516dcbd39520c163b8a1e7cb769ac6578a7
a-andrievskikh <sn0abg0@gmail.com>
2023-05-09 21:35:38 +0500

    chore: сomplete task 14 for 7 kyu

---
+++ src/codewars/7kyu/14.js
@@ -0,0 +1,8 @@
+function getFirstPython(list) {
+  for (let dev of list) {
+    if (dev.language === 'Python') {
+      return `${dev.firstName}, ${dev.country}`;
+    }
+  }
+  return `There will be no Python developers`;
+}
