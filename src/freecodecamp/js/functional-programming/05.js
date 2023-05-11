2e74e05533fc64ef378eac70cc3dbcdeb7eab3db
a-andrievskikh <sn0abg0@gmail.com>
2023-05-11 14:32:01 +0500

    chore: сomplete task 05 for Functional Programming

---
+++ src/freecodecamp/js/functional-programming/05.js
@@ -0,0 +1,11 @@
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer(num) {
+  num = fixedValue;
+  num++;
+  return num;
+
+  // Only change code above this line
+}
