22161bd4cdb4866f5de23745cfdd27dab508cbde
a-andrievskikh <sn0abg0@gmail.com>
2023-05-12 17:14:45 +0500

    chore: сomplete task 03 for JavaScript Algorithms and Data Structures Projects

---
+++ src/freecodecamp/js/javascript-algorithms-and-data-structures-projects/03.js
@@ -0,0 +1,44 @@
+let lookup = {
+  A: 'N',
+  B: 'O',
+  C: 'P',
+  D: 'Q',
+  E: 'R',
+  F: 'S',
+  G: 'T',
+  H: 'U',
+  I: 'V',
+  J: 'W',
+  K: 'X',
+  L: 'Y',
+  M: 'Z',
+  N: 'A',
+  O: 'B',
+  P: 'C',
+  Q: 'D',
+  R: 'E',
+  S: 'F',
+  T: 'G',
+  U: 'H',
+  V: 'I',
+  W: 'J',
+  X: 'K',
+  Y: 'L',
+  Z: 'M',
+};
+
+function rot13(encodedStr) {
+  let codeArr = encodedStr.split('');
+  let decodedArr = [];
+
+  decodedArr = codeArr.map(function (letter) {
+    if (lookup.hasOwnProperty(letter)) {
+      letter = lookup[letter];
+    }
+    return letter;
+  });
+
+  return decodedArr.join('');
+}
+
+rot13('SERR PBQR PNZC');
