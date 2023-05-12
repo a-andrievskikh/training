5524a24a19d3a62100b626d97a2d4e2168809efa
a-andrievskikh <sn0abg0@gmail.com>
2023-05-12 11:22:47 +0500

    chore: сomplete task 21 for Functional Programming

---
+++ src/freecodecamp/js/functional-programming/21.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+function urlSlug(title) {
+  return title
+    .toLowerCase()
+    .match(/[a-z]+/g)
+    .join('-');
+}
+// Only change code above this line
+urlSlug('A Mind Needs Books Like A Sword Needs A Whetstone');
