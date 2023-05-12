2be32f74e6b2bc618e174e9b8d487c0df5d69e0b
a-andrievskikh <sn0abg0@gmail.com>
2023-05-12 11:20:50 +0500

    chore: сomplete task 06 for Functional Programming

---
+++ src/freecodecamp/js/functional-programming/06.js
@@ -0,0 +1,26 @@
+// The global variable
+const bookList = [
+  'The Hound of the Baskervilles',
+  'On The Electrodynamics of Moving Bodies',
+  'Philosophiæ Naturalis Principia Mathematica',
+  'Disquisitiones Arithmeticae',
+];
+
+// Change code below this line
+function add([...bookList], bookName) {
+  bookList.push(bookName);
+  return bookList;
+
+  // Change code above this line
+}
+
+// Change code below this line
+function remove([...bookList], bookName) {
+  const book_index = bookList.indexOf(bookName);
+  if (book_index >= 0) {
+    bookList.splice(book_index, 1);
+    return bookList;
+
+    // Change code above this line
+  }
+}
