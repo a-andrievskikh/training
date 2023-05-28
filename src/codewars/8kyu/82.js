f030a9ada14d8089005341b015de7b212bf88049
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-05-28 14:36:05 +0500

    chore: сomplete task 82 for 8 kyu

---
+++ src/codewars/8kyu/82.js
@@ -0,0 +1,12 @@
+function leo(oscar) {
+  if (oscar === 88) {
+    return 'Leo finally won the oscar! Leo is happy'
+  }
+  if (oscar === 86) {
+    return 'Not even for Wolf of wallstreet?!'
+  }
+  if (oscar !== 88 && oscar !== 86 && oscar < 88) {
+    return 'When will you give Leo an Oscar?'
+  }
+  return 'Leo got one already!'
+}
