4690bb790f529b0bc096f67af726acaf3372131c
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-06-06 03:26:05 +0500

    chore: сomplete task 130 for 8 kyu

---
+++ src/codewars/8kyu-p2/130.js
@@ -0,0 +1,10 @@
+const yourFutureCareer = () => {
+  const career = Math.random()
+  if (career <= 0.32) {
+    return 'FrontEnd Developer'
+  } else if (career <= 0.65) {
+    return 'BackEnd Developer'
+  } else {
+    return 'Full-Stack Developer'
+  }
+}
