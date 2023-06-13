3c84abba019c9cf8b4ca474e474db9fcb4a5281f
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-06-13 18:40:53 +0500

    chore: сomplete task 32 for 7 kyu

---
+++ src/codewars/7kyu/32.js
@@ -0,0 +1,6 @@
+const Calculator = {}
+
+Calculator.add = (a, b) => a + b
+Calculator.subtract = (a, b) => a - b
+Calculator.multiply = (a, b) => a * b
+Calculator.divide = (a, b) => b ? ( a / b ) : false
