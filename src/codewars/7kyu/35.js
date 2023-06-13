f72688c3d223478809adc29dd511dd75a9d07d44
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-06-13 21:04:58 +0500

    chore: сomplete task 35 for 7 kyu

---
+++ src/codewars/7kyu/35.js
@@ -0,0 +1,11 @@
+function noIfsNoButs(a, b) {
+
+  switch (true) {
+    case a > b:
+      return `${a} is greater than ${b}`
+    case a === b:
+      return `${a} is equal to ${b}`
+    default:
+      return `${a} is smaller than ${b}`
+  }
+}
