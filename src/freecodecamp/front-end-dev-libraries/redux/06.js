ac5dae13ece40464c8d9c9012038965536aa6028
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-07-13 14:24:05 +0500

    chore: сomplete task 06 for Redux

---
+++ src/freecodecamp/front-end-dev-libraries/redux/06.js
@@ -0,0 +1,19 @@
+const defaultState = {
+  login: false,
+}
+
+const reducer = (state = defaultState, action) => {
+  // Change code below this line
+  return action.type === 'LOGIN'
+    ? {...state, login: true}
+    : state
+  // Change code above this line
+}
+
+const store = Redux.createStore(reducer)
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN',
+  }
+}
