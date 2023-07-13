34eefea61f4258c96c0244bfb3ee5de6cca04176
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-07-13 14:24:23 +0500

    chore: сomplete task 08 for Redux

---
+++ src/freecodecamp/front-end-dev-libraries/redux/08.js
@@ -0,0 +1,33 @@
+const defaultState = {
+  authenticated: false,
+}
+
+const LOGIN = 'LOGIN'
+const LOGOUT = 'LOGOUT'
+
+const authReducer = (state = defaultState, action) => {
+
+  switch (action.type) {
+    case LOGIN:
+      return {...state, authenticated: true}
+    case LOGOUT:
+      return {...state, authenticated: false}
+    default:
+      return state
+  }
+
+}
+
+const store = Redux.createStore(authReducer)
+
+const loginUser = () => {
+  return {
+    type: LOGIN,
+  }
+}
+
+const logoutUser = () => {
+  return {
+    type: LOGOUT,
+  }
+}
