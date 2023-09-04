f0abe67398bb8abea352f1c195e9661c7ed5c125
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-09-05 01:05:24 +0500

    chore: complete task 234 for 7 kyu

---
+++ src/codewars/7kyu-p3/34.js
@@ -0,0 +1,5 @@
+function reverseByCenter(s) {
+  return s.length % 2 === 0
+    ? `${s.slice(s.length / 2)}${s.slice(0, s.length / 2)}`
+    : `${s.slice(Math.ceil(s.length / 2))}${s[Math.floor(s.length / 2)]}${s.slice(0, Math.floor(s.length / 2))}`
+}
