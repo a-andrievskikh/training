835794f167b7d50a1d91126b929f975604af4120
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-09-07 12:26:12 +0500

    chore: complete task 241 for 7 kyu

---
+++ src/codewars/7kyu-p3/41.js
@@ -0,0 +1,15 @@
+function maxProduct(a) {
+  let max1 = -Infinity
+  let max2 = -Infinity
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > max1) {
+      max2 = max1
+      max1 = a[i]
+    } else if (a[i] > max2 && a[i] < max1) {
+      max2 = a[i]
+    }
+  }
+
+  return Math.max(...a) * max2
+}
