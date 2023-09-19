5f46e571bc794b2c19132d63c3aae17b952002a2
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-09-19 15:25:57 +0500

    chore: complete task 295 for 7 kyu

---
+++ src/codewars/7kyu-p3/95.js
@@ -0,0 +1,14 @@
+function toCurrency(price) {
+  const str = price.toString()
+  let result = ''
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    result = str[i] + result
+
+    if ((str.length - i) % 3 === 0 && i !== 0) {
+      result = ',' + result
+    }
+  }
+
+  return result
+}
