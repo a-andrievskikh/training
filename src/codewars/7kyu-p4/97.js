975e51c92a2287d1b9079279788105db83a5849c
a-andrievskikh <sn0abg0@gmail.com>
2024-04-09 17:44:17 +0500

    chore: complete 7 kyu - task 497

---
+++ src/codewars/7kyu-p4/97.js
@@ -0,0 +1,10 @@
+function countGrade(scores) {
+  return {
+    S: scores.filter(x => x === 100).length,
+    A: scores.filter(x => x < 100 && x >= 90).length,
+    B: scores.filter(x => x < 90 && x >= 80).length,
+    C: scores.filter(x => x < 80 && x >= 60).length,
+    D: scores.filter(x => x < 60 && x >= 0).length,
+    X: scores.filter(x => x === -1).length,
+  }
+}
